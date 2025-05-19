6bcba1f725b12aeae1d291f4000e23ab996366d3
Sahar Dastitash <sahar.dastitash@gmail.com>
2025-05-19 09:18:00 -0700

    Implementation of a queue using two stacks

--- Queue/QueueTwoStacks.cs
+++ Queue/QueueTwoStacks.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 
 namespace Queue
 {
+    // Implementation of a queue using two stacks
     public class QueueTwoStacks
     {
         private Stack<int> _inStack = new Stack<int>();
         private Stack<int> _outStack = new Stack<int>();
+        public int Count => _inStack.Count + _outStack.Count;
+
 
         public void Enqueue(int item)
         {
@@ -32,5 +35,26 @@ namespace Queue
 
             return _outStack.Pop();
         }
+
+        public int Peek()
+        {
+            if (_outStack.Count == 0)
+            {
+                while (_inStack.Count > 0)
+                {
+                    _outStack.Push(_inStack.Pop());
+                }
+
+                if (_outStack.Count == 0)
+                {
+                    throw new InvalidOperationException("Queue is empty.");
+                }
+            }
+
+            return _outStack.Peek();
+        }
+
+        public bool IsEmpty() => Count == 0;
+
     }
 }
